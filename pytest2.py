39b03dc6ec8c099b9675d2dfd673789f1ed58132
mstukas <stukas@avaya.com>
2019-02-27 15:35:36 -0700

    Add files via upload

---
+++ pytest2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys,os,time,re
+if __name__ == '__main__':
+    print ("scrap paper\n")
+
+    swversion = []
+    version = os.popen("uname -a")
+    cdir = os.popen("pwd")
+
+    for line in version:
+        swversion.append(line)
+     
+
+
+    for line in cdir:
+        swversion.append(line)
+
+
+
+    version.close()
+    cdir.close()
+
+    L = [print (line) for line in swversion]
+
+
+
+
